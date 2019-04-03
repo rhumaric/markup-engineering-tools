82c91a7145075416bab5030d01bc623d48681430
Romaric Pascal <hello@romaricpascal.is>
2019-04-03 08:35:16 +0100

    refactor: automatically prepend sass-true import to test files

--- test/compiler.js
+++ test/compiler.js
@@ -1,8 +1,16 @@
 require.extensions['.scss'] = function (module, filename) {
   return module._compile(`
 var sassTrue = require('sass-true');
+var fs = require('fs');
+var path = require('path');
+var filename = '${filename}';
+var data = fs.readFileSync(filename, {encoding: 'utf-8'});
+var TRUE_SETUP = "$true-terminal-output: false; @import 'true';";
 try {
-  sassTrue.runSass({ file: '${filename}', sass: require('sass')}, describe, it);
+  sassTrue.runSass({
+    data: TRUE_SETUP + data,
+    includePaths: [path.dirname(filename)],
+    sass: require('sass')}, describe, it);
 } catch (e) {
   console.log('Compilation failed');
   console.log(e.formatted);
