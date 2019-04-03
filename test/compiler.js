6119a81ddb674831d3d4eabd63e5ae52f2e3dc27
Romaric Pascal <hello@romaricpascal.is>
2019-04-03 08:21:26 +0100

    refactor: swap implementation of sass-true to support sass
    For now custom implementation on own repo, swap to main package if PR accepted

--- test/compiler.js
+++ test/compiler.js
@@ -1,6 +1,13 @@
 require.extensions['.scss'] = function (module, filename) {
   return module._compile(`
 var sassTrue = require('sass-true');
-sassTrue.runSass({ file: '${filename}' }, describe, it);
-  `, filename);
+try {
+  sassTrue.runSass({ file: '${filename}', sass: require('sass')}, describe, it);
+} catch (e) {
+  console.log('Compilation failed');
+  console.log(e.formatted);
+  throw e;
+};
+    `, filename);
+
 }
